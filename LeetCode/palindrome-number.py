4f2ddc38eba86009a9c1ffb1afbdc18502a3cd85
fardin <fardin@landsky.ai>
2024-11-26 22:34:54 -0500

    Fixed error
    Used value instead of index in line 9

--- LeetCode/palindrome-number.py
+++ LeetCode/palindrome-number.py
@@ -2,21 +2,19 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        elif x == 1:
+        elif len(str(x)) == 1:
             return True
         check = [int(i) for i in str(x)]
         inverse = check[::-1]
-
-        for i in check:
+        for i,value in enumerate(check):
             if check[i] == inverse[i]:
                 pass
             else:
                 return False
         return True
 
-
 if __name__ == "__main__":
     solution  = Solution()
-    number = 121
+    number = 999
     result = solution.isPalindrome(number)
     print(result)
