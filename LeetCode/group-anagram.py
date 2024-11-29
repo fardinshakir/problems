b3d7a86585311ce32c42a78884142fe2c5b5be8b
fardinshakir <fardin.shakir7@gmail.com>
2024-11-28 22:46:09 -0500

    Create group-anagram.py

---
+++ LeetCode/group-anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict[list]
+        for word in strs:
+            sorted_ = ''.join(sorted(word))
+            #anagrams[sorted_].append(word)
+            
+
+        #return anagrams
+if __name__ == "__main__":
+    solution  = Solution()
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    result = solution.groupAnagrams(strs)
+    print(result)
