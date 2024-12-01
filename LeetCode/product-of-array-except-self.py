7bbe24af105a5897e97a06a3df4b15a8be7b50fa
fardin <fardin.shakir7@gmail.com>
2024-12-01 01:54:32 -0500

    Create product-of-array-except-self.py

---
+++ LeetCode/product-of-array-except-self.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left, right = [1] * n, [1] * n
+        for i in range(1, n):
+            left[i] = left[i-1] * nums[i-1]
+        for i in range(n - 2, -1, -1):
+            right[i] = right[i + 1] * nums[i + 1]
+        print(left, right)
+if __name__ == "__main__":
+    solution  = Solution()
+    nums = [1,2,3,4]
+    result = solution.productExceptSelf(nums)
+    print(result)
