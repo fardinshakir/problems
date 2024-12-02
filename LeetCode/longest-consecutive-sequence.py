2cfa5877d381aa2367432a62d29bb41a5cb60bb9
fardin <fardin.shakir7@gmail.com>
2024-12-02 17:49:03 -0500

    Update longest-consecutive-sequence.py

--- LeetCode/longest-consecutive-sequence.py
+++ LeetCode/longest-consecutive-sequence.py
@@ -11,22 +11,9 @@ class Solution:
                 longest = max(longest, length)
         return longest
 
-"""
-        start = sorted_nums[0]
-        longest = 1
-        for i, n in enumerate(sorted_nums[1:]):
-            #print(f"{i}, {sorted_nums[1:][i]} - {start} = {sorted_nums[1:][i] - start}")
-            if sorted_nums[1:][i] - start == 1:
-                start = sorted_nums[1:][i]
-                longest += 1
-            else:
-                start = sorted_nums[1:][i]
-                longest = 1
-        return longest
-"""
 if __name__ == "__main__":
     solution  = Solution()
-    nums = [0,3,7,2,5,8,4,6,0,1, 1, 1]
+    nums = [0,3,7,2,5,8,4,6,0]
 
     result = solution.longestConsecutive(nums)
     print(result)
