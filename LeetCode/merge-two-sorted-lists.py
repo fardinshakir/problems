4bb27b4621bd7ebc26e70cdbfd6c13a6b298608f
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-06 14:54:43 -0500

    Update merge-two-sorted-lists.py

--- LeetCode/merge-two-sorted-lists.py
+++ LeetCode/merge-two-sorted-lists.py
@@ -7,30 +7,34 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = ListNode()
+        dummy = ListNode()
+        current = dummy
         while list1 and list2:
             if list1.val > list2.val:
-                cur.next = list2
+                current.next = list2
                 list2 = list2.next
             else:
-                cur.next = list1
+                current.next = list1
                 list1 = list1.next
-
-            cur = cur.next
+            current = current.next
 
         if list1:
-            cur.next = list1
+            current.next = list1
         else:
-            cur.next = list2
-
+            current.next = list2
         return dummy.next
-
+def createLinkedList(lst):
+    head = None
+    for val in reversed(lst):
+        head = ListNode(val, head)
+    return head
 
 if __name__ == "__main__":
     solution = Solution()
     list1 = [1,2,4]
     list2 = [1,3,4]
-    result = solution.mergeTwoLists(list1, list2)
+    result = solution.mergeTwoLists(createLinkedList(list1), createLinkedList(list2))
     print(result)
