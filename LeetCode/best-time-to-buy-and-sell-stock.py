1cca57642bd3adba11f0405ce13e6ac92cb550fc
fardinshakir <189673261+fardinshakir@users.noreply.github.com>
2024-12-07 20:11:08 -0500

    Create best-time-to-buy-and-sell-stock.py
    - Need to solve

---
+++ LeetCode/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+# Need to solve
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lowest = 7
+        highest = 7
+        for s in prices[1:]:
+            if s > highest
+if __name__ == "__main__":
+    solution = Solution()
+    prices = [7,1,5,3,6,4]
+    result = solution.maxProfit(prices)
+    print(result)
