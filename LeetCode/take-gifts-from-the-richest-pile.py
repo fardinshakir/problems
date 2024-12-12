591c5a7da5d4282dd34582928e4744019da9e7b9
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-12 16:20:06 -0500

    Update take-gifts-from-the-richest-pile.py

--- LeetCode/take-gifts-from-the-richest-pile.py
+++ LeetCode/take-gifts-from-the-richest-pile.py
@@ -1,6 +1,7 @@
 from typing import List
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        """
         while k:
             biggest = 0
             piles = [0]
@@ -18,10 +19,18 @@ class Solution:
             gifts[piles[0]] = biggest
             k -=1
         return sum(gifts)
+        """
 
+        while k:
+            gifts = sorted(gifts)[::-1]
+            pile = gifts[0]
+            gifts.pop(0)
+            gifts.append(int(pile ** 0.5))
+            k -=1
+        return sum(gifts)
 if __name__ == "__main__":
     solution = Solution()
-    gifts = [1,1,1,1]
+    gifts = [25,64,9,4,100]
     k = 4
     result = solution.pickGifts(gifts, k)
     print(result)
