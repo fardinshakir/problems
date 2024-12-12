bab4bda2aa45eaeadc1b142535436700d48db88d
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-12 16:12:14 -0500

    Update valid-palindrome.py
    - Two pointer solution

--- LeetCode/valid-palindrome.py
+++ LeetCode/valid-palindrome.py
@@ -1,15 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        mstr = ""
-        for c in s.lower():
-            if c.isalnum():
-                mstr+= c
-        return mstr == mstr[::-1]
+        #mstr = ""
+        #for c in s.lower():
+        #    if c.isalnum():
+        #        mstr+= c
+        #return mstr == mstr[::-1]
 
-
+        #return s.lower() == s[::-1].lower()
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum(): l+=1
+            while l < r and not s[r].isalnum(): r-=1
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+        return True
 
 if __name__ == "__main__":
     solution = Solution()
-    s = "Racecar"
+    s = "A man, a plan, a canal: Panama"
     result = solution.isPalindrome(s)
     print(result)
