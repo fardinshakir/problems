8c68ad51b7470391b1fcffb0a9cb2fe46053eff8
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-12 16:13:58 -0500

    Create take-gifts-from-the-richest-pile.py
    - First solution

---
+++ LeetCode/take-gifts-from-the-richest-pile.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        while k:
+            biggest = 0
+            piles = [0]
+            for i, j in enumerate(gifts):
+                if gifts[i] > biggest:
+                    biggest = gifts[i]
+                    piles = [i]
+                    continue
+                elif gifts[i] < biggest:
+                    continue
+                elif gifts[i] == biggest:
+                    piles.append(i)
+                    continue
+            biggest = int(biggest ** 0.5)
+            gifts[piles[0]] = biggest
+            k -=1
+        return sum(gifts)
+
+if __name__ == "__main__":
+    solution = Solution()
+    gifts = [1,1,1,1]
+    k = 4
+    result = solution.pickGifts(gifts, k)
+    print(result)
