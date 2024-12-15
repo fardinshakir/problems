35ae1d1af0304ba7eadca938882f7a2a2caae2dd
Fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-14 19:34:22 -0500

    Create fibonacci-number.py

---
+++ LeetCode/fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        prev, cur = 0, 1
+        for q in range(0, n):
+            oldcur = cur
+            cur = cur + prev
+            prev = oldcur
+        return prev
+
+if __name__ == "__main__":
+    solution  = Solution()
+    n = 4
+    result = solution.fib(b)
+    print(result)
