74dcbaac6c142c4da5cfdde1224cf3112a40494e
fardinshakir <189673261+fardinshakir@users.noreply.github.com>
2024-12-15 19:33:28 -0500

    Update best-time-to-buy-and-sell-stock.py

--- LeetCode/best-time-to-buy-and-sell-stock.py
+++ LeetCode/best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,40 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        lowest = 7
-        highest = 7
-        for s in prices[1:]:
-            if s > highest
-        # iterage through and find the biggest and smallest for each s
-        # left and right point? 
+        """
+        O(N^2)
+        best = 0
+        for i, start_day in enumerate(prices):
+            for others in range(i+1, len(prices)):
+                current_value = -(start_day - prices[others])
+                if current_value > best:
+
+                    best = current_value
+        return best
+        """
+        """
+        O(N) slow
+        min_price = float('inf')
+        max_price = 0
+        for current in prices:
+            min_price = min(min_price, current)
+            profit = current - min_price
+            max_price = max(max_price, profit)
+        return max_price
+        """
+        """
+        O(N) faster
+        """
+        min_price = prices[0]
+        max_profit = 0
+        for current in prices:
+            if current < min_price:
+                min_price = current
+            profit = current - min_price
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
 if __name__ == "__main__":
     solution = Solution()
     prices = [7,1,5,3,6,4]
