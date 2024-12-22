b3bec4ab3667ce5158043417293e83cfaf25d49c
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-22 02:48:05 -0500

    Update single-number.py

--- LeetCode/single-number.py
+++ LeetCode/single-number.py
@@ -1,5 +1,12 @@
+from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         for n in nums:
             if nums.count(n) == 1:
                 return n
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1, 1, 2, 6, 6]
+    result = solution.singleNumber(nums)
+    print(result)
