67d8d615e9feed0fed4b6954d042c2d764758fbe
fardin <189673261+fardinshakir@users.noreply.github.com>
2024-12-24 00:05:52 -0500

    Create climbing-stairs.py

---
+++ LeetCode/climbing-stairs.py
@@ -0,0 +1,32 @@
+# Need to review
+class Solution:
+    def climbStairs(self, n: int) -> int:
+# Dynamic programming
+        """
+                if n <= 3:
+                    return n
+                pre1 = 3
+                pre2 = 2
+                cur = 0
+                for _ in range(3, n):
+                    cur = pre1 + pre2
+                    print(pre1, pre2, cur)
+                    pre2 = pre1
+                    pre1 = cur
+                return cur
+        """
+# Recursion
+"""
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        my_val = self.climbStairs(n-1) + self.climbStairs(n-2)
+        print(my_val)
+        return my_val
+"""
+if __name__ == "__main__":
+    solution = Solution()
+    n = 44
+    result = solution.climbStairs(n)
+    print(result)
